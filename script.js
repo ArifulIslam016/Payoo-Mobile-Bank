15fa95535838d613d9b8316da6af5425d4bd3aea
Ariful Islam <arifulq234@gmail.com>
2025-08-23 02:27:16 +0600

    login page completed

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById("loginBtn").addEventListener("click", function (eo) {
   eo.preventDefault();
+  const userPhone = 1610854092;
+  const userPin = 1234;
+  const phoneNo = document.getElementById("phone-no").value;
+  const convertedPhoneNo = parseInt(phoneNo);
+  const pinNo = document.getElementById("pin-no").value;
+  const convertedPinNo = parseInt(pinNo);
+  console.log(convertedPhoneNo, convertedPinNo);
+  if (userPhone === convertedPhoneNo && userPin === convertedPinNo) {
+    console.log("login suceed");
+  } else {
+    alert("Invalid Credintial");
+  }
 });
