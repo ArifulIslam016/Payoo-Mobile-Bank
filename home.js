3fa6d4c4adca24624bc5619a2090f977c64df1f6
Ariful Islam <arifulq234@gmail.com>
2025-08-24 20:40:33 +0600

    Add money functionality completed

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const pinNo = 1234;
+
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
@@ -5,10 +7,25 @@ document
     const selectedBank = document.getElementById("selected-bank").value;
     const bankAcNO = document.getElementById("bank-ac-no").value;
     const amount = parseInt(document.getElementById("amount-to-add").value);
-    const pin = document.getElementById("pin-no").value;
+    const pin = parseInt(document.getElementById("pin-no").value);
     const mainBalance = parseInt(
       document.getElementById("main-balance").innerText
     );
+    // if (selectedBank === " ") {
+    //   alert("Please Select a bank");
+    //   return;
+    // }
+    if (bankAcNO.length !== 11) {
+      alert("Provide a valid Account No");
+      return;
+    }
+    if (pin !== pinNo) {
+      alert("Wrong pin");
+      return;
+    }
     const totalBalance = amount + mainBalance;
     document.getElementById("main-balance").innerText = totalBalance;
+
   });
+  //  togoling feature
+  
