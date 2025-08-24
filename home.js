6d7810c77bc56c348bcd6d051b2213089531da9f
Ariful Islam <arifulq234@gmail.com>
2025-08-25 01:03:19 +0600

    Cashout functionality completed

--- home.js
+++ home.js
@@ -25,7 +25,41 @@ document
     }
     const totalBalance = amount + mainBalance;
     document.getElementById("main-balance").innerText = totalBalance;
-
   });
-  //  togoling feature
-  
+
+// CashOut form funtionality
+document.getElementById("cashOut-btn").addEventListener("click", function (E) {
+  E.preventDefault();
+  const agentNo = document.getElementById("agent-no").value;
+  const withDrawAmount = parseInt(
+    document.getElementById("amount-to-withdraw").value
+  );
+  const pinNumber = document.getElementById("pin-number").value;
+  if (agentNo.length !== 11) {
+    alert("Provide a vaid Agent Number");
+    return;
+  }
+  if (pinNumber !== pinNo) {
+    alert("Please provide a valid pin Number");
+    return;
+  }
+
+  const mainBalance = parseInt(
+    document.getElementById("main-balance").innerText
+  );
+  console.log(mainBalance);
+
+  const newBalance = mainBalance - withDrawAmount;
+  document.getElementById("main-balance").innerText = newBalance;
+});
+
+//  togoling feature
+document.getElementById("add-btn").addEventListener("click", function () {
+  document.getElementById("CashOut-form-parent").style.display = "none";
+  document.getElementById("addMoney-form-parent").style.display = "block";
+});
+
+document.getElementById("Cout-btn").addEventListener("click", function () {
+  document.getElementById("addMoney-form-parent").style.display = "none";
+  document.getElementById("CashOut-form-parent").style.display = "block";
+});
